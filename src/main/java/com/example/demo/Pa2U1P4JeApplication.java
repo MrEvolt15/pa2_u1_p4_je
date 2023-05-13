a4a1a8532c90c24f7722ffabe09c67a6e4f758b8
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-12 19:29:01 -0500

    Deficion de Spring Framework

--- src/main/java/com/example/demo/Pa2U1P4JeApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4JeApplication.java
@@ -13,7 +13,10 @@ public class Pa2U1P4JeApplication  implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		System.out.println("Hola mundo");
+		/*Spring Framework: esto es un framework que permite la construccion/desarrollo
+		 * de aplicaciones basadas en Spring*/
 	}
 
 }
